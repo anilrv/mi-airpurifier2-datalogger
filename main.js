f1ffa68138b49601d8053465dc150993cc377849
Anil R V <anil.chesswon4u@gmail.com>
2021-01-01 00:50:09 +0530

    code added

---
+++ main.js
@@ -0,0 +1,81 @@
+const dotenv = require('dotenv');
+dotenv.config();
+
+const AirPurifier = require('miio/lib/devices/air-purifier');
+const miio = require('miio');
+
+
+const ONE_SECOND = 1;
+const ONE_MILLI_SECOND = 1000;
+const ONE_SECOND_MS = ONE_SECOND * ONE_MILLI_SECOND;
+const PurifierToken = process.env.TOKEN;
+
+
+PatchMiioDeviceModel();
+Initilize();
+
+async function Initilize() {
+    let ipArray = await getMiDeviceIpAddress(ONE_SECOND * 30);
+    let devices = [];
+    ipArray.forEach(ip => {
+        devices.push(await connectToDevice(ip, PurifierToken));
+    });
+    devices.forEach(device => {
+        logDeviceParameters(device);
+        setInterval(() => {
+            logDeviceParameters(device);
+        }, ONE_SECOND_MS * 60);
+        device.on('pm2.5Changed', pm2_5 => {
+            console.log('New AQI:' + pm2_5);
+        });
+    });
+}
+
+async function getMiDeviceIpAddress(timeout) {
+    let ipArray = [];
+    return new Promise((resolve) => {
+        const devices = miio.devices();
+        devices.on('available', device => {
+            console.log('Found device - ' + device.address);
+            ipArray.push(device.address);
+            // Assuming only 1 miio device is there resolving quickly.
+            resolve(ipArray);
+        });
+        setTimeout(function () {
+            resolve(ipArray);
+        }, timeout * ONE_MILLI_SECOND);
+    });
+}
+
+function connectToDevice(address, token) {
+    return new Promise((resolve, reject) => {
+        miio.device({
+            address, token
+        }).then(device => {
+            resolve(device);
+        }).catch(err => {
+            console.log(err);
+            reject(err);
+        });
+    });
+}
+
+async function logDeviceParameters(device) {
+    if (device.matches('type:air-purifier')) {
+        console.log('\n');
+        console.log('Air purifier on:', await device.power());
+        console.log('Mode:', await device.mode());
+        const temp = await device.temperature();
+        console.log('Temperature:', temp.celsius);
+
+        const rh = await device.relativeHumidity();
+        console.log('Relative humidity:', rh);
+
+        const aqi = await device.pm2_5();
+        console.log('Air Quality Index:', aqi);
+    }
+}
+
+function PatchMiioDeviceModel() {
+    miio.models['zhimi.airpurifier.mc1'] = AirPurifier;
+}
---
+++ readme.md
@@ -0,0 +1,12 @@
+Token Download method.
+
+1.Install Xiaomi Home App on your iOS or Android phone. Then add new device. Be sure you can control it via app with no problems.
+2.When using Android phone, purge Xiaomi Home App.
+3.Download apk version 5.4.54. It can be easily found on google. Ether download it directly to your phone or copy it later form PC.
+Finally, install
+4.Open app, login, then make sure your purifier is available on the listing. If everything looks fine just kill the app.
+5.Browse your phone file system for specific file: SmartHome/log/plug_DeviceManager/*.txt
+6.Search for token within the mentioned log file. Voilà!
+
+From: https://mic22.medium.com/how-to-get-token-of-xiaomi-air-purifier-h3-oct-2020-47d0f02451c3
+Mi Home APK: https://www.apkmirror.com/apk/xiaomi-inc/mihome/mihome-5-4-45-release/mi-home-5-4-45-android-apk-download/download/
